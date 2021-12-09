aaa0111b9fb424f5627aec4bf43721c42c7f1d52
罗婵 <905794630@qq.com>
2021-12-09 22:08:31 +0800

    修改pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,18 +21,18 @@ module.exports = {
   }],
   deploy : {
     production : {
-      key: '',
-      user : '罗婵',
+      key: '~/.ssh/id_rsa',
+      user : 'root',
       //服务器ip
       host : '8.142.66.4',
       //远程分支
-      ref  : 'master',
+      ref  : 'origin/master',
       //仓库地址
       repo : 'git@github.com:heythanks/nocode-backend.git',
       //项目要部署到服务器上的位置，PM2默认会部署到该路径的source子文件夹下
       path : '/usr/local/html',
       //服务器下载到最新的代码之后要执行命令
-      'post-deploy' : 'yarn && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'yarn && pm2 restart ecosystem.config.js --env production',
     }
   }
 };
